cf2d836bf8e404b884d2e926f4c7a0dfdfbe531c
UserPL022Pc02 <UserPL022Pc02@SLFRPL022PC02.ensino.atec.local>
2016-10-07 15:36:17 +0100

    d

---
+++ src/exercicios071016/treino.java
@@ -0,0 +1,45 @@
+package exercicios071016;
+
+import java.util.Scanner;
+
+public class treino {
+	
+	public treino(){	
+		exercicio2();
+	}
+	
+	
+	private void exercicio1(){
+		System.out.println("Num 1: ");
+		int num1 = (new Scanner(System.in)).nextInt();
+		System.out.println("Num 2: ");
+		int num2 = (new Scanner(System.in)).nextInt();
+		
+		if(num1>num2){
+			System.out.println("Num 1 é o maior");
+		}else{
+			System.out.println("Num 2 é o maior");
+		}
+			
+		
+	}
+	
+	private void exercicio2(){
+		System.out.println("Num 1: ");
+		int num1 = (new Scanner(System.in)).nextInt();
+		System.out.println("Num 2: ");
+		int num2 = (new Scanner(System.in)).nextInt();
+		System.out.println("Num 3: ");
+		int num3 = (new Scanner(System.in)).nextInt();
+		if(num1>num2 && num1>num3){
+			System.out.println("Num 1 é o maior");
+		}
+		if(num2>num1 && num2>num3){
+			System.out.println("Num 2 é o maior");
+		}
+		if(num3>num1 && num3>num2){
+			System.out.println("Num 3 é o maior");
+		}
+		
+	}
+}
